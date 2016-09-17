8ed52bd13ebca3077f8d6280439ccf9be5b32b57
Kroekpong Sakulchai <kroekpong@gmail.com>
2016-09-17 22:56:40 +0700

    Modify SecurityConfiguration.java for develop first

--- src/main/java/th/co/tac/kms/web/controller/KioskProfileReportController.java
+++ src/main/java/th/co/tac/kms/web/controller/KioskProfileReportController.java
@@ -1,18 +1,14 @@
 package th.co.tac.kms.web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import th.co.tac.kms.web.controller.model.KioskRegisterInfo;
 
 @Controller
 public class KioskProfileReportController extends AbstractController  {
 
 	@RequestMapping(value = { "/report-profile" }, method = RequestMethod.GET)
-	public String init(@ModelAttribute KioskRegisterInfo keyform , ModelMap model) {
+	public String init() {
 //		model.addAttribute("keyform",  new KioskRegisterInfo());
 		return "report.profile";
 	}
