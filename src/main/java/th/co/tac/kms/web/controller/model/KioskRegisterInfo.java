895f61100bff5947c86140c20d880e70dc57b892
jeeraphan <jeeraphan@xp-link.com>
2016-09-17 19:36:10 +0700

    Kiosk Register Controller

---
+++ src/main/java/th/co/tac/kms/web/controller/model/KioskRegisterInfo.java
@@ -0,0 +1,221 @@
+package th.co.tac.kms.web.controller.model;
+
+public class KioskRegisterInfo {
+
+	private String networkMa;
+	private String mobileMa;
+	private String referentTaName;
+	private String customerType;
+	private String companyName;
+	private String lastName;
+	
+//	Kiosk Location Profile
+	private String vendorName;
+	private String kioskId;
+	private String kioskStatus;
+	
+	//ข้อมูลเจ้าของพื้นที่ (แก้ไขได้)
+	private String areaTitleName;
+	private String areaFirstName;
+	private String areaLastName;
+	private String areaMobileNo;
+	
+
+//	ข้อมูลสถานที่ติดตั้งตู้เติมเงิน (แก้ไขได้)
+	private String kioskAreaType;
+	private String kioskAreaAddress;
+	private String kioskAreaMoo;
+	private String kioskAreaSoi;
+	private String kioskAreaRoad;
+	private String kioskAreaProvice;
+	private String kioskAreaDistrict;
+	private String kioskAreaTambon;
+	private String kioskAreaPostalCode;
+	private String kioskAreaRemarkAddr;
+	
+	
+	public String getNetworkMa() {
+		return networkMa;
+	}
+
+	public void setNetworkMa(String networkMa) {
+		this.networkMa = networkMa;
+	}
+
+	public String getMobileMa() {
+		return mobileMa;
+	}
+
+	public void setMobileMa(String mobileMa) {
+		this.mobileMa = mobileMa;
+	}
+
+	public String getReferentTaName() {
+		return referentTaName;
+	}
+
+	public void setReferentTaName(String referentTaName) {
+		this.referentTaName = referentTaName;
+	}
+
+	public String getCustomerType() {
+		return customerType;
+	}
+
+	public void setCustomerType(String customerType) {
+		this.customerType = customerType;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getVendorName() {
+		return vendorName;
+	}
+
+	public void setVendorName(String vendorName) {
+		this.vendorName = vendorName;
+	}
+
+	public String getKioskId() {
+		return kioskId;
+	}
+
+	public void setKioskId(String kioskId) {
+		this.kioskId = kioskId;
+	}
+
+	public String getKioskStatus() {
+		return kioskStatus;
+	}
+
+	public void setKioskStatus(String kioskStatus) {
+		this.kioskStatus = kioskStatus;
+	}
+
+	public String getAreaTitleName() {
+		return areaTitleName;
+	}
+
+	public void setAreaTitleName(String areaTitleName) {
+		this.areaTitleName = areaTitleName;
+	}
+
+	public String getAreaFirstName() {
+		return areaFirstName;
+	}
+
+	public void setAreaFirstName(String areaFirstName) {
+		this.areaFirstName = areaFirstName;
+	}
+
+	public String getAreaLastName() {
+		return areaLastName;
+	}
+
+	public void setAreaLastName(String areaLastName) {
+		this.areaLastName = areaLastName;
+	}
+
+	public String getAreaMobileNo() {
+		return areaMobileNo;
+	}
+
+	public void setAreaMobileNo(String areaMobileNo) {
+		this.areaMobileNo = areaMobileNo;
+	}
+
+	public String getKioskAreaType() {
+		return kioskAreaType;
+	}
+
+	public void setKioskAreaType(String kioskAreaType) {
+		this.kioskAreaType = kioskAreaType;
+	}
+
+	public String getKioskAreaAddress() {
+		return kioskAreaAddress;
+	}
+
+	public void setKioskAreaAddress(String kioskAreaAddress) {
+		this.kioskAreaAddress = kioskAreaAddress;
+	}
+
+	public String getKioskAreaMoo() {
+		return kioskAreaMoo;
+	}
+
+	public void setKioskAreaMoo(String kioskAreaMoo) {
+		this.kioskAreaMoo = kioskAreaMoo;
+	}
+
+	public String getKioskAreaSoi() {
+		return kioskAreaSoi;
+	}
+
+	public void setKioskAreaSoi(String kioskAreaSoi) {
+		this.kioskAreaSoi = kioskAreaSoi;
+	}
+
+	public String getKioskAreaRoad() {
+		return kioskAreaRoad;
+	}
+
+	public void setKioskAreaRoad(String kioskAreaRoad) {
+		this.kioskAreaRoad = kioskAreaRoad;
+	}
+
+	public String getKioskAreaProvice() {
+		return kioskAreaProvice;
+	}
+
+	public void setKioskAreaProvice(String kioskAreaProvice) {
+		this.kioskAreaProvice = kioskAreaProvice;
+	}
+
+	public String getKioskAreaDistrict() {
+		return kioskAreaDistrict;
+	}
+
+	public void setKioskAreaDistrict(String kioskAreaDistrict) {
+		this.kioskAreaDistrict = kioskAreaDistrict;
+	}
+
+	public String getKioskAreaTambon() {
+		return kioskAreaTambon;
+	}
+
+	public void setKioskAreaTambon(String kioskAreaTambon) {
+		this.kioskAreaTambon = kioskAreaTambon;
+	}
+
+	public String getKioskAreaPostalCode() {
+		return kioskAreaPostalCode;
+	}
+
+	public void setKioskAreaPostalCode(String kioskAreaPostalCode) {
+		this.kioskAreaPostalCode = kioskAreaPostalCode;
+	}
+
+	public String getKioskAreaRemarkAddr() {
+		return kioskAreaRemarkAddr;
+	}
+
+	public void setKioskAreaRemarkAddr(String kioskAreaRemarkAddr) {
+		this.kioskAreaRemarkAddr = kioskAreaRemarkAddr;
+	}
+
+}
