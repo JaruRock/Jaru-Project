01040e36ba2398c18a0aa36150e50cd953cc6b20
nuttawut <bootnuttawig@gmail.com>
2016-10-20 20:31:05 +0700

    Init combobox province

--- src/main/webapp/resources/js/master-location.js
+++ src/main/webapp/resources/js/master-location.js
@@ -1,5 +1,23 @@
 $(function() {
 
-	$.get("./rest/1/tambons", function(data) { console.log($.parseJSON(data)); });
-	
+	$("#province").select2({
+		ajax : {
+			url : './rest/provinces'
+			, dataType : 'json'
+			, type: "GET"
+			, data: function (params) {
+				return {
+					q : params.term
+				};
+			}
+			, processResults : function(data) {
+				return {
+					results : data.items
+				};
+			}
+			, minimumInputLength: 1
+			, cache: true
+		}
+	});
+
 });
